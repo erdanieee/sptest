33ad980a62218eb413a27fa2525d9f5cd0c64e4c
Carlos <loucerac@unican.es>
2019-06-06 15:39:45 +0200

    Add description and package files.

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from setuptools import setup, find_packages
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(
-      # mandatory
-      name="sptest",
-      # mandatory
-      version="0.1",
-      # mandatory
-      author_email="carlos.loucera@juntadeandalucia.es",
-      packages=['sptest'],
-      package_data={},
-      install_requires=['numpy', 'pandas', 'click', 'dotenv', 'scikit-learn'],
-      entry_points={
-        'console_scripts': ['sptest = sptest.cli:start']
-      }
+  # mandatory
+  name="sptest",
+  description='Spanish Test with 1k Genome training.',
+  long_description=readme(),
+  # mandatory
+  version="0.1",
+  # mandatory
+  author_email="carlos.loucera@juntadeandalucia.es",
+  packages=['sptest'],
+  include_package_data=True,
+  install_requires=['numpy', 'pandas', 'click', 'dotenv', 'scikit-learn'],
+  entry_points={
+    'console_scripts': ['sptest = sptest.cli:start']
+  }
 )
