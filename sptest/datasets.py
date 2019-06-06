0a9a4f2213c7a411c2d4401765341f094456f084
Carlos <loucerac@unican.es>
2019-06-06 15:40:47 +0200

    Add functions to load test files/folders.

--- sptest/datasets.py
+++ sptest/datasets.py
@@ -5,6 +5,7 @@ email: carlos.loucera@juntadeandalucia.es
 Spanish Test I/O module.
 """
 
+from pathlib import Path
 import pandas as pd
 
 from .utils import get_data_path
@@ -28,3 +29,37 @@ def load_training():
     y_train_bin = y_train["nationality"].values.ravel() == "Spanish"
 
     return X_train, y_train, y_train_bin
+
+def load_test_file(fpath):
+
+    fpath = Path(fpath)
+    # load .Q file as pandas DataFrame
+    df = pd.read_csv(fpath, header=None, sep=" ")
+
+    if df.shape[0] == 0:
+        raise Exception("No samples found in {}".format(fpath))
+
+    # Only the first sample must be evaluated
+    return df.iloc[0, :].copy()
+
+def load_test_folder(inputpath):
+
+    inputpath = Path(inputpath)
+
+    groups = []
+    sample_names = []
+    frames = []
+    for fpath in inputpath.glob("**/*.Q"):
+    #     print(fpath)
+        sample_name = fpath.parent.name
+        group = fpath.parent.parent.name
+
+        try:
+            df = load_test_file(fpath)
+            groups.append(group)
+            sample_names.append(sample_name)
+            frames.append(df.iloc[0, :].copy())
+
+    test_v2 = pd.concat(frames, axis=1, ignore_index=True).T
+    test_v2.index = sample_names
+    test_v2.head()
