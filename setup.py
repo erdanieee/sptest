4a610cb9ea98e12f08bd150fbdf8990d51b0d6c7
Carlos <loucerac@unican.es>
2019-06-07 17:29:23 +0200

    Reformat code.

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 from setuptools import setup, find_packages
 
+
 def readme():
     with open('README.md') as f:
         return f.read()
 
+
 setup(
-  # mandatory
-  name="sptest",
-  description='Spanish Test with 1k Genome training.',
-  long_description=readme(),
-  # mandatory
-  version="0.1",
-  # mandatory
-  author_email="carlos.loucera@juntadeandalucia.es",
-  packages=['sptest'],
-  include_package_data=True,
-  install_requires=['numpy', 'pandas', 'click', 'dotenv', 'scikit-learn'],
-  entry_points={
-    'console_scripts': ['sptest = sptest.cli:start']
-  }
+    # mandatory
+    name="sptest",
+    description='Spanish Test with 1k Genome training.',
+    long_description=readme(),
+    # mandatory
+    version="0.1",
+    # mandatory
+    author_email="carlos.loucera@juntadeandalucia.es",
+    packages=['sptest'],
+    include_package_data=True,
+    install_requires=[
+        'numpy', 'pandas', 'click', 'python-dotenv', 'scikit-learn',
+        'matplotlib', 'scikit-optimize', 'xgboost'
+    ],
+    entry_points={
+        'console_scripts': ['sptest = sptest.cli:main']
+    }
 )
